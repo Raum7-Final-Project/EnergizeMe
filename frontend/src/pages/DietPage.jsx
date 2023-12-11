262b5486051fb29c281ae2c554fa7c11c700dd3b
Leonard <leonard.kehl@gmail.com>
2023-12-11 13:41:20 +0100

    add test image for recipe and lorem for tips

--- frontend/src/pages/DietPage.jsx
+++ frontend/src/pages/DietPage.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
+import SaladImage from "../assets/images/salad-test.jpg";
 
 const DietPage = () => {
   const STYLE = {
     container: `bg-white h-screen grid grid-rows-[50px_30px,1fr] gap-1 p-4 overflow-auto`,
     h2: `text-4xl text-center p-2 text-[#C3C3B8] font-bold`,
     headerContainer: `grid grid-cols-[_repeat(5,_1fr)] grid-rows-1 border-b-gray-300 border-b-2 text-xl text-bold  font-bold tracking-wide gap-4`,
-    recipeContainer: `box-border h-1/4 w-auto p4 border-4 mt-1 mb-4`,
-    tipsContainer: `box-border h-1/4 w-auto p4 border-4 mt-1 mb-4`,
+    recipeContainer: `box-border h-1/4 w-auto p4 border-4 mt-1 mb-4 rounded-lg p-7`,
+    recipeImage: ``,
+    tipsContainer: `box-border h-1/4 w-auto p4 border-4 mt-1 mb-4 rounded-lg p-2`,
   };
 
   
@@ -31,19 +33,25 @@ const handleTab = (tab)=>{
       <div className={activeTab == "recipe" ? "" : "hidden"}>
       <p className="font-bold text-lg">Rezepte</p>
       <div className={STYLE.recipeContainer}>
-        <p>text</p>
+        <h3>Salate</h3>
+        <img src={SaladImage} className={STYLE.recipeImage} />
 
       </div>
       <div className={STYLE.recipeContainer}>
-        <p>text</p>
+       <h3>Salate</h3>
+       <img src={SaladImage} className={STYLE.recipeImage} />
 
       </div>
       <div className={STYLE.recipeContainer}>
-        <p>text</p>
+        <h3>Salate</h3>
+        <img src={SaladImage} className={STYLE.recipeImage} />
+
 
       </div>
       <div className={STYLE.recipeContainer}>
-        <p>text</p>
+        <h3>Salate</h3>
+        <img src={SaladImage} className={STYLE.recipeImage} />
+
 
       </div>
       </div>
@@ -52,23 +60,23 @@ const handleTab = (tab)=>{
       <div className={activeTab == "tips" ? "" : "hidden"}>
       <p className="font-bold text-lg">Tips</p>
       <div className={STYLE.tipsContainer}>
-        <p>text</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptates quo suscipit ea officia soluta minus illum molestias nihil cum!</p>
 
       </div>
       <div className={STYLE.tipsContainer}>
-        <p>text</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure, libero voluptatibus. Ducimus accusamus, fugit ab suscipit numquam corporis quod laborum ratione, veniam alias adipisci hic excepturi atque itaque repellendus eveniet!</p>
 
       </div>
       <div className={STYLE.tipsContainer}>
-        <p>text</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, nesciunt?</p>
 
       </div> 
       <div className={STYLE.tipsContainer}>
-        <p>text</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In quo rerum sed id eveniet quibusdam pariatur illo. Amet fugit nulla ea neque ad commodi sed ex quas quos magnam eius odio, aperiam dolorem, cupiditate quibusdam.</p>
 
       </div> 
       <div className={STYLE.tipsContainer}>
-        <p>text</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut repudiandae ipsum odio incidunt deleniti dolores.</p>
 
       </div>
       </div>
